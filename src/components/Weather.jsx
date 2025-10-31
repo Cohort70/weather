2744f54006b3b5d0b4e8d7edcfa4687bcc62a775
edward <edwardlfish@gmail.com>
2025-10-31 16:39:57 +0200

    First commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,12 @@
+const Weather = () => {
+    return (
+        <div className={'infoWeath'}>
+            <p>Location: de, Berlin</p>
+            <p>Temp: 10</p>
+            <p>Pressure: 1010</p>
+            <p>Sunset: 18:00</p>
+        </div>
+    );
+};
+
+export default Weather;
